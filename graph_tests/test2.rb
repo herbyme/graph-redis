e43f947cbfe46f05c082a9e426b087d3ffb5ce75
Omar Qunsul <omar.qunsul@gmail.com>
2014-07-24 00:11:31 +0200

    fixing tests

---
+++ graph_tests/test2.rb
@@ -0,0 +1,28 @@
+# Minimum spanning tree
+
+require 'redis'
+
+redis = Redis.new
+
+describe 'Minimum spanning tree' do
+
+  it 'should calculate the correct minimum spanning tree' do
+
+    redis.flushdb
+    redis.gvertex 'graph1', 'a', 'b', 'c', 'd', 'e'
+    redis.gedge 'graph1', 'a', 'b', 1
+    redis.gedge 'graph1', 'c', 'b', 1
+    redis.gedge 'graph1', 'd', 'c', 1
+    redis.gedge 'graph1', 'd', 'e', 1
+    redis.gedge 'graph1', 'a', 'e', 3
+    redis.gedge 'graph1', 'a', 'd', 100
+
+    redis.gmintree('graph1', 'graph2')
+    min_tree = redis.gedges('graph1')
+
+    puts min_tree.inspect
+
+  end
+
+end
+
